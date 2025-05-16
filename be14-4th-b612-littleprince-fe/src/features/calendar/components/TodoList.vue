5d38eac7e3adf21ebd63ab0915674b03d51b5cfa
kkkwid <lsj9057@daum.net>
2025-05-17 03:05:41 +0900

    feat: 플랜 등록 폼 구현

---
+++ be14-4th-b612-littleprince-fe/src/features/calendar/components/TodoList.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { ref } from 'vue'
+
+const todos = ref([
+  {
+    task_id: 1,
+    content: '요구사항 명세서 작성 끝내기',
+    is_checked: 'N',
+    project_id: null
+  },
+  {
+    task_id: 2,
+    content: '인프런 스프링부트 1강 듣기',
+    is_checked: 'N',
+    project_id: 5,
+    project_title: '투두 프로젝트'
+  },
+  {
+    task_id: 3,
+    content: '피그마 작업 마무리하기',
+    is_checked: 'N',
+    project_id: null
+  },
+  {
+    task_id: 4,
+    content: 'ERD 작업 시작하기',
+    is_checked: 'N',
+    project_id: 5,
+    project_title: '투두 프로젝트'
+  },
+  {
+    task_id: 5,
+    content: '프론트엔드 초기 세팅 끝내기',
+    is_checked: 'Y',
+    project_id: null
+  }
+])
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 max-h-[300px] overflow-y-auto pr-1">
+    <div
+        v-for="todo in todos"
+        :key="todo.task_id"
+        class="group flex items-center justify-between px-3 py-2 rounded-md bg-white/10 backdrop-blur-sm"
+    >
+      <div class="flex items-center gap-2">
+        <input
+            type="checkbox"
+            v-model="todo.is_checked"
+            true-value="Y"
+            false-value="N"
+            class="w-4 h-4 accent-purple-400 bg-white/20 border-white/30 rounded"
+        />
+        <span :class="{ 'line-through opacity-60': todo.is_checked === 'Y' }" class="text-sm">
+          {{ todo.content }}
+        </span>
+      </div>
+
+      <!-- 삭제 버튼 (trash.png) -->
+      <button class="hidden group-hover:block transition bg-transparent p-0 border-none shadow-none">
+        <img
+            src="@/assets/icons/trash.png"
+            alt="삭제"
+            class="w-4 h-4 object-contain"
+        />
+      </button>
+    </div>
+  </div>
+</template>
+
+
+
---
+++ be14-4th-b612-littleprince-fe/src/features/calendar/views/PlanWriteForm.vue
@@ -0,0 +1,83 @@
+<script setup>
+import { ref } from 'vue'
+import TodoList from "@/features/calendar/components/TodoList.vue";
+import calendarBg from "@/assets/images/calendar-background.jpg";
+
+const toggle = ref(false)
+</script>
+
+<template>
+  <div
+      :style="{ backgroundImage: `url(${calendarBg})` }"
+      class="scene-container relative bg-cover bg-center w-full h-screen"
+  >
+    <div
+        class="fixed top-[100px] bottom-[100px] right-[5%] w-[25%] h-[70%]
+             bg-[#E8D0FF]/30 backdrop-blur-md rounded-2xl p-6 flex flex-col gap-4 text-white shadow-lg z-10"
+    >
+      <!-- 날짜 + 토글 -->
+      <div class="flex items-center justify-between">
+        <div class="flex items-center gap-2">
+          <label class="px-3 py-[6px] bg-[#C9C3E3]/40 rounded-xl text-sm text-black">날짜</label>
+
+          <!-- 시작 날짜 -->
+          <input
+              type="date"
+              class="px-3 py-[6px] bg-[#C9C3E3]/40 text-black placeholder-white/60 rounded-xl text-sm border-none focus:outline-none appearance-none"
+          />
+          <span
+              v-if="toggle"
+              class="text-sm">~</span>
+
+          <!-- 종료 날짜 -->
+          <input
+              v-if="toggle"
+              type="date"
+              class="px-3 py-[6px] bg-[#C9C3E3]/40 text-black placeholder-white/60 rounded-xl text-sm border-none focus:outline-none appearance-none"
+          />
+        </div>
+        <!-- 토글 -->
+        <div @click="toggle = !toggle" class="w-12 h-6 rounded-full relative cursor-pointer transition-all duration-300"
+             :class="toggle ? 'bg-[#60A5FA]/60' : 'bg-gray-400'">
+          <div
+              class="absolute top-[2px] w-5 h-5 bg-white rounded-full shadow transition-all duration-300"
+              :class="toggle ? 'right-[2px]' : 'left-[2px]'"
+          ></div>
+        </div>
+      </div>
+      <!-- 제목 입력 -->
+      <div class="flex flex-col">
+        <input
+            v-if="toggle"
+            type="text"
+            placeholder="제목을 입력하세요."
+            class="w-30% px-3 py-2 rounded-xl bg-[#C9C3E3]/40 text-black placeholder-[#161717]/50 border-none focus:outline-none text-sm"
+        />
+      </div>
+      <div class="w-full h-[1px] bg-white/40"></div>
+      <!-- Todo 리스트 -->
+      <TodoList />
+      <!-- 버튼 영역 -->
+
+        <div class="flex gap-2">
+          <button class="bg-[#C9C3E3]/30 hover:bg-[#A49CAC]/60 text-black px-3 py-1 rounded-xl text-sm
+         border border-white/10 transition">+</button>
+        </div>
+        <div class="flex gap-2">
+          <button
+              class="bg-[#C9C3E3]/30 hover:bg-[#A49CAC]/60 text-black px-1 py-1 rounded-xl text-sm
+         border border-white/10 transition"
+          >AI 생성하기</button>
+        </div>
+      <div class="flex justify-between mt-auto">
+        <div class="flex gap-2 ml-auto">
+          <button class="bg-[#C9C3E3]/30 hover:bg-[#A49CAC]/60 text-black px-3 py-1 rounded-xl text-sm
+         border border-white/10 transition">취소</button>
+          <button class="bg-[#C9C3E3]/30 hover:bg-[#A49CAC]/60 text-black px-3 py-1 rounded-xl text-sm
+         border border-white/10 transition">확인</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
