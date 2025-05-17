dabd1c5ec4fb3d1c791ba074716cfd9d1ded95bd
Cho-Hyun-Seung <toki0327@naver.com>
2025-05-17 16:11:14 +0900

    fix: '/' 경로에서만 어린왕자가 보이게 변경

--- be14-4th-b612-littleprince-fe/src/App.vue
+++ be14-4th-b612-littleprince-fe/src/App.vue
@@ -3,6 +3,7 @@ import SideBar from '@/components/layout/SideBar.vue';
 
 import { useRoute } from 'vue-router';
 import { computed } from 'vue';
+import PlanetScene from "@/features/main/components/PlanetScene.vue";
 
 const route = useRoute();
 const useLayout = computed(() => route.meta.layout !== 'none');
@@ -11,6 +12,7 @@ const useLayout = computed(() => route.meta.layout !== 'none');
 <template>
   <div id="app" class="flex h-screen w-screen overflow-hidden">
     <SideBar v-if="useLayout" />
+    <PlanetScene/>
     <div class="flex-1" :class="{ layout: useLayout }">
       <router-view />
     </div>
