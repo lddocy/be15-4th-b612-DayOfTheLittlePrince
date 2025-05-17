1a6cf9ddcc4c4bdffe44a9cebe1ff8cea99db82f
lddocy <jkcy911219@naver.com>
2025-05-17 10:32:01 +0900

    chore: design tokens(스타일 가이드) 설정

--- be14-4th-b612-littleprince-fe/src/main.js
+++ be14-4th-b612-littleprince-fe/src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import './assets/styles/tailwind.css'
+import './assets/styles/design-tokens.css'
 
 const app = createApp(App)
 
