de831baa9c19a184fcbac62523134066bd254256
Cho-Hyun-Seung <toki0327@naver.com>
2025-05-18 20:50:56 +0900

    fix: 회원가입 로직 추가 #9

--- be14-4th-b612-littleprince-fe/src/features/user/views/SignupView.vue
+++ be14-4th-b612-littleprince-fe/src/features/user/views/SignupView.vue
@@ -2,7 +2,9 @@
 
 import MainLogo from "@/features/user/components/MainLogo.vue";
 import {ref} from "vue";
+import {useRouter} from "vue-router";
 
+const router = useRouter();
 
 const email = ref('');
 const password = ref('');
@@ -11,16 +13,36 @@ const confirmPassword = ref('');
 const handleSignup = async () => {
   try {
     /* 1. 공백 제거*/
+    email.value = email.value.trim();
+    password.value = password.value.trim();
+    confirmPassword.value = confirmPassword.value.trim();
 
     /* 2. 입력 값 검증 */
+    const email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    if (!email_regex.test(email.value)) {
+      throw new Error("유효하지 않은 이메일입니다.");
+    }
+
+    if (email.value === '' || password.value === '') {
+      throw new Error("이메일과 패스워드를 입력해주세요.");
+    }
+
+    if (password.value !== confirmPassword.value) {
+      throw new Error("비밀번호가 일치하지 않습니다.");
+    }
 
     /* 3. 입력 전송 */
 
+    const formData = {email: email.value, password: password.value, confirmPassword: confirmPassword.value}
+
+    // const resp = await signup(formData);
 
     /* 4. 성공 시 로그인 페이지로 이동*/
+    await router.replace("/login")
 
   }catch(e){
-    console.log(e)
+    console.log(e.message)
+    alert(e.message)
   }
 }
 </script>
