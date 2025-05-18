bb56db9bbb50a3df8a28896b4e9c7f53ca369432
lddocy <jkcy911219@naver.com>
2025-05-18 21:56:59 +0900

    feat: BGM 재생 기능 구현

--- be14-4th-b612-littleprince-fe/src/App.vue
+++ be14-4th-b612-littleprince-fe/src/App.vue
@@ -1,23 +1,31 @@
 <script setup>
-import SideBar from '@/components/layout/SideBar.vue';
-
 import { useRoute } from 'vue-router';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
+
+import SideBar from '@/components/layout/SideBar.vue';
 import PlanetScene from "@/features/main/components/PlanetScene.vue";
+import BgmPlayer from './components/common/BgmPlayer.vue';
 
 const route = useRoute();
 const useLayout = computed(() => route.meta.layout !== 'none');
 </script>
 
 <template>
-  <div id="app" class="flex h-screen w-screen overflow-hidden">
-    <SideBar v-if="useLayout" />
-<!-- '/' 경로에서는 배경으로 안쓰이게 -->
-    <PlanetScene :class="{'planet-front' : route.path === '/'}"/>
-    <div class="flex-1" :class="{ layout: useLayout }">
-      <router-view />
+    <div id="app" class="flex h-screen w-screen overflow-hidden relative">
+        <SideBar v-if="useLayout" />
+
+        <div class="absolute bottom-6 left-[90px] lg:left-[160px] md:left-[120px] z-50">
+            <BgmPlayer />
+        </div>
+
+        <!-- '/' 경로에서는 배경으로 안쓰이게 -->
+        <PlanetScene :class="{'planet-front' : route.path === '/'}"/>
+
+        <div class="flex-1" :class="{ layout: useLayout }">
+            <router-view />
+        </div>
+
     </div>
-  </div>
 </template>
 
 <style>
