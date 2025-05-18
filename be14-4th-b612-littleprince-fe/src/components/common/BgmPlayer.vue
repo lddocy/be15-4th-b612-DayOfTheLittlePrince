9cabc09624a71021b892e45ad9bf2791017e3e83
lddocy <jkcy911219@naver.com>
2025-05-18 22:12:46 +0900

    fix: 불필요한 조건문 제거

--- be14-4th-b612-littleprince-fe/src/components/common/BgmPlayer.vue
+++ be14-4th-b612-littleprince-fe/src/components/common/BgmPlayer.vue
@@ -22,19 +22,17 @@ const isPlaying = ref(false);
 let audio = null;
 
 onMounted(() => {
-    if (props.enabled === 'Y') {
-        audio = new Audio('/audio/background.mp3');
-        audio.loop = true;
-        audio.volume = 0.5;
-        audio.play()
-            .then(() => {
-                isPlaying.value = true;
-            })
-            .catch(e => {
-                console.warn('자동 재생 실패:', e);
-                isPlaying.value = false;
-            });
-    }
+    audio = new Audio('/audio/background.mp3');
+    audio.loop = true;
+    audio.volume = 0.5;
+    audio.play()
+        .then(() => {
+            isPlaying.value = true;
+        })
+        .catch(e => {
+            console.warn('자동 재생 실패:', e);
+            isPlaying.value = false;
+        });
 });
 
 onBeforeUnmount(() => {
