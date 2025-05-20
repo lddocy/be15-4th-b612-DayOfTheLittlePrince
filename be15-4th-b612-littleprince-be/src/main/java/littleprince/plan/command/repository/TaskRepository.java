38a0c917518f3750a1603a223a5e6c0dd6bac4e5
oyk0510 <youkyungoh0510@naver.com>
2025-05-20 15:07:16 +0900

    feat: findByTaskIdAndMemberId 메서드 생성

--- be15-4th-b612-littleprince-be/src/main/java/littleprince/plan/command/repository/TaskRepository.java
+++ be15-4th-b612-littleprince-be/src/main/java/littleprince/plan/command/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package littleprince.plan.command.repository;
 
 import littleprince.plan.command.domain.aggregate.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findByTaskIdAndMemberId(Long taskId, Long memberId);
 }
