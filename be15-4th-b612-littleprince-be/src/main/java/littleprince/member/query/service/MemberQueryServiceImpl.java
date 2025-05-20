04719b37baacadd84361921a057ddda4f54a974e
Cho-Hyun-Seung <toki0327@naver.com>
2025-05-20 12:08:42 +0900

    fix: 멤버 엔티티 생성

---
+++ be15-4th-b612-littleprince-be/src/main/java/littleprince/member/query/service/MemberQueryServiceImpl.java
@@ -0,0 +1,11 @@
+package littleprince.member.query.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberQueryServiceImpl implements MemberQueryService{
+
+
+}
