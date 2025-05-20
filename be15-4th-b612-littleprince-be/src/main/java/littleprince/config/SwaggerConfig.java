d3848a0a1a7ae9e706efeab2f9eb934922f63ed0
Cho-Hyun-Seung <toki0327@naver.com>
2025-05-20 14:38:45 +0900

    chore: 스웨거 설정 추가 #108

---
+++ be15-4th-b612-littleprince-be/src/main/java/littleprince/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package littleprince.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(swaggerInfo());
+    }
+
+    private Info swaggerInfo() {
+        return new Info()
+                .title("B612 API Docs")
+                .description("B612 API 명세서")
+                .version("1.0.0");
+    }
+}
