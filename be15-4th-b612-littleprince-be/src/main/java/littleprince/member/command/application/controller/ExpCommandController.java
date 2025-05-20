72c4d6cce1c7926f362c97ceb4f47fd3f83bdbdc
Joonspar <sdwang15@naver.com>
2025-05-20 18:37:27 +0900

    fix: 경험치 조회 구현

--- be15-4th-b612-littleprince-be/src/main/java/littleprince/member/command/application/controller/ExpCommandController.java
+++ be15-4th-b612-littleprince-be/src/main/java/littleprince/member/command/application/controller/ExpCommandController.java
@@ -1,5 +1,7 @@
 package littleprince.member.command.application.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import littleprince.common.dto.ApiResponse;
 import littleprince.config.security.model.CustomUserDetail;
 import littleprince.member.command.application.dto.request.ExpRequest;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "행성")
 @RestController
 @RequestMapping("/exp")
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class ExpCommandController {
 
     private final MemberCommandService memberCommandService;
 
+    @Operation(summary = "행성 경험치 추가", description = "사용자는 경험치를 얻을 수 있다.")
     @PostMapping
     public ResponseEntity<ApiResponse<ExpResponse>> addExp(@AuthenticationPrincipal CustomUserDetail user,
                                                            @RequestBody ExpRequest request) {
--- be15-4th-b612-littleprince-be/src/main/java/littleprince/member/query/controller/ExpQueryController.java
+++ be15-4th-b612-littleprince-be/src/main/java/littleprince/member/query/controller/ExpQueryController.java
@@ -1,5 +1,7 @@
 package littleprince.member.query.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import littleprince.common.dto.ApiResponse;
 import littleprince.config.security.model.CustomUserDetail;
 import littleprince.member.query.dto.response.CurExpResponse;
@@ -9,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "프로필")
 @RestController
 @RequestMapping("/curexp")
 @RequiredArgsConstructor
@@ -16,6 +19,7 @@ public class ExpQueryController {
 
     private final ExpQueryService expQueryService;
 
+    @Operation(summary = "경험치 조회",description = "사용자는 자신의 경험치를 조회할 수 있다.")
     @GetMapping
     public ResponseEntity<ApiResponse<CurExpResponse>> getMyExp(@AuthenticationPrincipal CustomUserDetail user) {
         Long memberId = user.getMemberId();
