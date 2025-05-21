3585ad9bab80867380e01bfb658aef8e576836f0
kkkwid <lsj9057@daum.net>
2025-05-21 19:55:26 +0900

    feat: 연동 api.js 생성

---
+++ be14-4th-b612-littleprince-fe/src/features/calendar/api.js
@@ -0,0 +1,133 @@
+import api from "@/plugins/axios.js";
+
+/* 장기 투두 생성 */
+export function createLongTodo(accessToken) {
+    return api.post(
+        '/plans/long-todos',
+        {},
+        {
+            headers: { Authorization: `Bearer ${accessToken}` },
+        }
+    );
+}
+
+/* 장기 투두 삭제 */
+export function deleteLongTodo(accessToken, projectId) {
+    return api.delete(`/plans/projects/${projectId}`, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+}
+
+/* 장기 프로젝트 하위 체크리스트 생성 */
+export function createProjectTasks(accessToken, projectId) {
+    return api.post(
+        `/plans/project-tasks/${projectId}/tasks`,
+        {},
+        {
+            headers: { Authorization: `Bearer ${accessToken}` },
+        }
+    );
+}
+
+/* 장기 투두 체크 상태 토글 */
+export function toggleProjectTaskCheck(accessToken, taskId) {
+    return api.patch(
+        `/plans/project-tasks/${taskId}/check`,
+        {},
+        {
+            headers: { Authorization: `Bearer ${accessToken}` },
+        }
+    );
+}
+
+/* 장기 투두 체크리스트 항목 삭제 */
+export function deleteProjectTask(accessToken, taskId) {
+    return api.delete(`/plans/project-tasks/${taskId}`, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+}
+
+/* 단기 투두 생성 */
+export function createShortTodo(accessToken) {
+    return api.post(
+        `/plans/short-todo`,
+        {},
+        {
+            headers: { Authorization: `Bearer ${accessToken}` },
+        }
+    );
+}
+
+/* 단기 투두 삭제 */
+export function deleteShortTodo(accessToken, taskId) {
+    return api.delete(`/plans/short-todos/${taskId}`, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+}
+
+/* 단기 투두 체크 상태 토글 */
+export function toggleCheck(accessToken, taskId) {
+    return api.patch(
+        `/plans/short-todos/${taskId}/check`,
+        {},
+        {
+            headers: { Authorization: `Bearer ${accessToken}` },
+        }
+    );
+}
+
+/* 장기 플랜 전체 조회 */
+export function getLongPlan(accessToken){
+    return api.get('/plan/long', {
+        params: {},
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+}
+
+/* 장기 플랜 목록 조회 */
+export function getLongList(accessToken, date){
+    return api.get(`/plan/long/${date}`, {
+        params: {},
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+}
+
+/* 장기 플랜 상세 조회 */
+export function getLongDetail(accessToken, date, projectId){
+    return api.get(`/plan/long/${date}/${projectId}`, {
+        params: {},
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+}
+
+/* 단기 리스트 조회 */
+export function getShortList(accessToken, date){
+    return api.get(`/plan/short/${date}/todo`, {
+        params: {},
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+}
+
+/* 단기 플랜 날짜 여부 조회 */
+export function getShortDates(accessToken){
+    return api.get(`/plan/short`, {
+        params: {},
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+}
