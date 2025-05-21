d56500bcb2c3fe58762893cc61cc645b15955b67
kkkwid <lsj9057@daum.net>
2025-05-21 15:19:24 +0900

    feat: 획득 경험치 조회 구현

--- be15-4th-b612-littleprince-be/src/main/java/littleprince/member/query/dto/GetExpHistoryDTO.java
+++ be15-4th-b612-littleprince-be/src/main/java/littleprince/member/query/dto/GetExpHistoryDTO.java
@@ -3,9 +3,14 @@ package littleprince.member.query.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.sql.Date;
+
 @Getter
 @Builder
 public class GetExpHistoryDTO {
     private Long historyId;
+    private Long projectId;
+    private String title;
+    private Date endDate;
     private int expPoint;
 }
--- be15-4th-b612-littleprince-be/src/main/java/littleprince/member/query/mapper/ExpHistoryQueryMapper.java
+++ be15-4th-b612-littleprince-be/src/main/java/littleprince/member/query/mapper/ExpHistoryQueryMapper.java
@@ -1,9 +1,15 @@
 package littleprince.member.query.mapper;
 
+import littleprince.member.query.dto.GetExpHistoryDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface ExpHistoryQueryMapper {
     boolean existsTodayHistory(@Param("memberId") Long memberId);
+
+    /* 획득 경험치 조회 */
+    List<GetExpHistoryDTO> getExpHistory(Long memberId);
 }
