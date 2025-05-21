8a014e4b00922357b70aa592105f6e1e5573bf68
oyk0510 <youkyungoh0510@naver.com>
2025-05-21 15:14:25 +0900

    feat: 장기 투두 체크 토글 API 구현

--- be15-4th-b612-littleprince-be/src/main/java/littleprince/plan/command/application/controller/ProjectTaskCommandController.java
+++ be15-4th-b612-littleprince-be/src/main/java/littleprince/plan/command/application/controller/ProjectTaskCommandController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/plans/projects/{projectId}/tasks")
+@RequestMapping("/plans/project-tasks")
 public class ProjectTaskCommandController {
 
     private final ProjectTaskCommandService projectTaskCommandService;
 
-    @PostMapping
+    @PostMapping("/{projectId}/tasks")
     @Operation(summary = "장기 프로젝트 하위 체크리스트 생성", description = "특정 프로젝트에 여러 개의 할 일을 추가합니다.")
     public ResponseEntity<ApiResponse<Void>> createProjectTasks(
             @AuthenticationPrincipal CustomUserDetail userDetail,
@@ -26,6 +26,16 @@ public class ProjectTaskCommandController {
             @RequestBody @Valid CreateProjectTaskRequestDto dto
             ){
         projectTaskCommandService.createProjectTasks(userDetail.getMemberId(),projectId, dto);
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @PatchMapping("/{taskId}/check")
+    @Operation(summary = "장기 투두 체크 상태 토글", description = "장기 프로젝트에 속한 체크리스트 항목의 체크 상태를 토글합니다.")
+    public ResponseEntity<ApiResponse<Void>> toggleProjectTaskCheck(
+            @AuthenticationPrincipal CustomUserDetail userDetail,
+            @PathVariable Long taskId
+    ) {
+        projectTaskCommandService.toggleProjectTaskCheck(userDetail.getMemberId(), taskId);
         return ResponseEntity.ok(ApiResponse.success(null));
     }
 }
