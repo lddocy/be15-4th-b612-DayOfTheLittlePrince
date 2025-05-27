1acf13f22dcf587e08c7ec804ea367ec65f4aef0
Cho-Hyun-Seung <toki0327@naver.com>
2025-05-27 12:14:22 +0900

    fix: 도커 멀티플랫폼으로 변경

--- be15-4th-b612-littleprince-be/docker.ps1
+++ be15-4th-b612-littleprince-be/docker.ps1
@@ -1,2 +1,2 @@
-docker build -t toki0327/k8s-b612-backend .
-docker push toki0327/k8s-b612-backend
+docker buildx create --use --name multi-builder
+docker buildx build --platform linux/amd64,linux/arm64 -t toki0327/k8s-b612-backend:latest --push .
--- be15-4th-b612-littleprince-fe/docker.ps1
+++ be15-4th-b612-littleprince-fe/docker.ps1
@@ -1,2 +1,2 @@
-docker build -t toki0327/k8s-b612-frontend .
-docker push toki0327/k8s-b612-frontend
+docker buildx create --use --name multi-builder
+docker buildx build --platform linux/amd64,linux/arm64 -t toki0327/k8s-b612-frontend:latest --push .
