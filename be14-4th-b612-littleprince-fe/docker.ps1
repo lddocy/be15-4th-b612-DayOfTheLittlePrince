729b57aa09962e50a006b7bb4f645cab31672ad7
Cho-Hyun-Seung <toki0327@naver.com>
2025-05-27 09:20:52 +0900

    fix: 파일명 삭제

--- be14-4th-b612-littleprince-fe/docker.ps1
+++
@@ -1,2 +0,0 @@
-docker build -t toki0327/k8s-b612-frontend .
-docker push toki0327/k8s-b612-frontend
--- be14-4th-b612-littleprince-fe/src/stores/ui.js
+++
@@ -1,7 +0,0 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-
-export const useUIStore = defineStore('ui', () => {
-    const isCapturing = ref(false)
-    return { isCapturing }
-})
